a884afebe8a2235d1ae00655010b2c6b3664cb70
cranedroesch <andrewcd@gmail.com>
2018-05-14 14:49:45 -0400

    getyhat now generalized to multinet

--- R/getYhat.R
+++ R/getYhat.R
@@ -1,28 +1,18 @@
 # function to compute fitted values, potentially for subsets of the data
 
 getYhat <- function(pl, hlay, param, y, ydm, fe_var, nlayers){ 
-  if (length(nlayers)>1){
-    if (!is.null(fe_var)){
-      Z <- foreach(i = 1:length(nlayers), .combine = cbind) %do% {
-        hlay[[i]][[length(hlay[[i]])]]
-      }
-      Z <- cbind(param, as.matrix(Z))
-      Zdm <- demeanlist(Z, list(fe_var))
-      B <- foreach(i = 1:length(nlayers), .combine = c) %do% {pl[[i]]$beta}
-      fe <- (y-ydm) - MatMult(as.matrix(Z)-Zdm, as.matrix(c(pl$beta_param, B)))
-      yhat <- MatMult(Z, c(pl$beta_param, B)) + fe    
-    } else {
-      stop("not implemented yet for no FE's")
-    }            
+  if (!is.null(fe_var)){
+    Z <- foreach(i = 1:length(nlayers), .combine = cbind) %do% {
+      hlay[[i]][[length(hlay[[i]])]]
+    }
+    Z <- cbind(param, as.matrix(Z))
+    Zdm <- demeanlist(Z, list(fe_var))
+    B <- foreach(i = 1:length(nlayers), .combine = c) %do% {pl[[i]]$beta}
+    fe <- (y-ydm) - MatMult(as.matrix(Z)-Zdm, as.matrix(c(pl$beta_param, B)))
+    yhat <- MatMult(Z, c(pl$beta_param, B)) + fe    
   } else {
-    if (!is.null(fe_var)){
-      Zdm <- demeanlist(as.matrix(hlay[[length(hlay)]]), list(fe_var))
-      fe <- (y-ydm) - MatMult(as.matrix(hlay[[length(hlay)]])-Zdm, as.matrix(c(pl$beta_param, pl$beta)))
-      yhat <- MatMult(hlay[[length(hlay)]], c(pl$beta_param, pl$beta)) + fe    
-    } else {
-      yhat <- MatMult(hlay[[length(hlay)]], c(pl$beta_param, pl$beta))
-    }      
-  }
+    stop("not implemented yet for no FE's")
+  }            
   return(as.numeric(yhat))
 }
 
